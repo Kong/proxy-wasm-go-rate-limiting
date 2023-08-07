50d79e2db79fab7519863afbbad7f6fb0a212496
Hisham Muhammad <hisham@gobolinux.org>
2023-08-07 11:46:26 -0300

    tests(demo): update demo to latest Kong Gateway

--- test/demo.sh
+++ test/demo.sh
@@ -1,70 +1,75 @@
 #!/usr/bin/env bash
 set -x
 
-# Configuration:
-
-modules=(
-   "rate-limiting"
-)
-
-DEMO_KONG_CONTAINER="${DEMO_KONG_CONTAINER:-kong-wasmx}"
-DEMO_KONG_IMAGE="${DEMO_KONG_IMAGE:-kong/kong:3.2.0.0-ubuntu}"
+DEMO_KONG_CONTAINER="${DEMO_KONG_CONTAINER:-kong-wasm}"
+DEMO_KONG_IMAGE="${DEMO_KONG_IMAGE:-kong/kong:nightly}"
 
 ################################################################################
 
+if [[ "$1" == "stop" ]]
+then
+    docker stop $DEMO_KONG_CONTAINER
+    docker rm $DEMO_KONG_CONTAINER
+    exit 0
+fi
+
+### Build filter ###############################################################
+
+(
+    cd ..
+    make || exit 1
+) || exit 1
+
+### Copy filter to wasm/ #######################################################
+
 mkdir -p wasm
+
+cp -a ../*.wasm wasm/
 
 script_dir=$(dirname $(realpath $0))
 
-for module in "${modules[@]}"
-do
-   cp ../$module.wasm wasm/
-done
+### Start container ############################################################
 
 docker stop $DEMO_KONG_CONTAINER
 docker rm $DEMO_KONG_CONTAINER
 
-wasm_modules=""
-for module in "${modules[@]}"
-do
-   if [ "$wasm_modules" = "" ]
-   then
-      wasm_modules="/wasm/$module.wasm"
-   else
-      wasm_modules="$wasm_modules,/wasm/$module.wasm"
-   fi
-done
+# Config trick to access localhost in a local Docker test,
+# in case you want to edit your config/demo.yml to target
+# a localhost server rather than mockbin.org:
+#
+# access_localhost="--add-host=host.docker.internal:$(ip -j address | jq -r '[ .[] | select(.ifname | test("^[ew]")) | .addr_info[] | select(.family == "inet") | .local ][0]')"
+access_localhost=""
 
 docker run -d --name "$DEMO_KONG_CONTAINER" \
-  -v "$script_dir/config:/kong/config/" \
-  -v "$script_dir/wasm:/wasm" \
-  -e "KONG_LOG_LEVEL=info" \
-  -e "KONG_DATABASE=off" \
-  -e "KONG_NGINX_WORKER_PROCESSES=1" \
-  -e "KONG_DECLARATIVE_CONFIG=/kong/config/demo.yml" \
-  -e "KONG_NGINX_WASM_SHM_KONG_WASM_RATE_LIMITING_COUNTERS=12m" \
-  -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" \
-  -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
-  -e "KONG_PROXY_ERROR_LOG=/dev/stderr" \
-  -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
-  -e "KONG_ADMIN_LISTEN=0.0.0.0:8001" \
-  -e "KONG_ADMIN_GUI_URL=http://localhost:8002" \
-  -e "KONG_WASM_MODULES=$wasm_modules" \
-  -e KONG_LICENSE_DATA \
-  -p 8000:8000 \
-  -p 8443:8443 \
-  -p 8001:8001 \
-  -p 8444:8444 \
-  -p 8002:8002 \
-  -p 8445:8445 \
-  -p 8003:8003 \
-  -p 8004:8004 \
-   "$DEMO_KONG_IMAGE"
+    $access_localhost \
+    -v "$script_dir/config:/kong/config/" \
+    -v "$script_dir/wasm:/wasm" \
+    -e "KONG_LOG_LEVEL=info" \
+    -e "KONG_DATABASE=off" \
+    -e "KONG_DECLARATIVE_CONFIG=/kong/config/demo.yml" \
+    -e "KONG_NGINX_WASM_SHM_KONG_WASM_RATE_LIMITING_COUNTERS=12m" \
+    -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" \
+    -e "KONG_PROXY_ERROR_LOG=/dev/stderr" \
+    -e "KONG_WASM=on" \
+    -e "KONG_WASM_FILTERS_PATH=/wasm" \
+    -e KONG_LICENSE_DATA \
+    -p 8000:8000 \
+    -p 8443:8443 \
+    -p 8001:8001 \
+    -p 8444:8444 \
+     "$DEMO_KONG_IMAGE"
+
+### Show configuration #########################################################
 
 cat config/demo.yml
 
-sleep 10
+sleep 5
 
+### Issue requests #############################################################
+
+http :8000/echo
+http :8000/echo
+http :8000/echo
 http :8000/echo
 
 #docker stop $DEMO_KONG_CONTAINER
